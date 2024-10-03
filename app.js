3d32905e138a0fff9a2ca967d33cd583fd8a8401
shikhapathania4 <112324439+shikhapathania4@users.noreply.github.com>
2024-10-03 12:51:42 +0400

    Port change

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
